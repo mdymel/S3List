b6f1fe4de5f92b2064a1907caa8fadb7c7e854d9
Michal Dymel <michal.dymel@wayn.com>
2015-05-15 15:59:07 +0200

    initial commit

---
+++ S3List/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Configuration;
+using Amazon;
+using Amazon.S3;
+
+namespace S3List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bucket = ConfigurationManager.AppSettings["bucket"];
+
+            var s3Client = new AmazonS3Client(RegionEndpoint.EUCentral1);
+            var list = s3Client.ListObjects(bucket);
+            foreach (var s3Object in list.S3Objects)
+            {
+                Console.WriteLine(s3Object.Key);
+            }
+        }
+    }
+}
